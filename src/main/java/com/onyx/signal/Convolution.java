befc8f25ede396d3c277669b65bb6a49385c3fd5
psambit9791 <sambitpaul1992@gmail.com>
2019-11-28 14:44:53 +0000

    Restructured Project with Maven, Added Convolution code

---
+++ src/main/java/com/onyx/signal/Convolution.java
@@ -0,0 +1,39 @@
+package com.onyx.signal;
+
+import org.apache.commons.math3.util.MathArrays;
+
+public class Convolution {
+    private double[] signal;
+    private double[] kernel;
+    private double[] output;
+    private String mode;
+
+    public Convolution(double[] s, double[] w, String mode) {
+        this.signal = s;
+        this.kernel = w;
+        this.mode = mode;
+    }
+
+    public void convolve() {
+        double[] temp = MathArrays.convolve(this.signal, this.kernel);
+        if (this.mode.equals("full")) {
+            this.output = temp;
+        }
+        else if (this.mode.equals("same")) {
+            this.output = new double[this.signal.length];
+            int iterator = 1;
+            for (int i=0; i<this.output.length; i++) {
+                this.output[i] = temp[iterator];
+                iterator++;
+            }
+        }
+        else if (this.mode.equals("valid")) {
+            this.output = new double[this.signal.length - this.kernel.length + 1];
+            int iterator = this.kernel.length-1;;
+            for (int i=0; i<this.output.length; i++) {
+                this.output[i] = temp[iterator];
+                iterator++;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/onyx/signal/TestDetrend.java
@@ -0,0 +1,36 @@
+package com.onyx.signal;
+
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import com.onyx.signal.Detrend;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class TestDetrend {
+
+    @Test
+    public void linearDetrendTest() {
+        double[] original = {1.0, 2.0, 3.0, 4.0, 5.0};
+        final double[] result = {0.0, 0.0, 0.0, 0.0, 0.0};
+
+        Detrend d1 = new Detrend(original, "linear");
+        d1.detrendSignal();
+        double[] out = d1.getDetrendedSignal();
+        assertTrue(Arrays.equals(out, result));
+    }
+
+    @Test
+    public void constantDetrendTest() {
+        double[] original = {1.0, 2.0, 3.0, 4.0, 5.0};
+        final double[] result = {-2.0, -1.0, 0.0, 1.0, 2.0};
+
+        Detrend d1 = new Detrend(original, "constant");
+        d1.detrendSignal();
+        double[] out = d1.getDetrendedSignal();
+        assertTrue(Arrays.equals(out, result));
+    }
+}
