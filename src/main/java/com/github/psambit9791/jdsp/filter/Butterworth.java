47e57e128594939e81f8ab89e9cea68086557a06
psambit9791 <sambitpaul1992@gmail.com>
2020-06-02 18:51:18 +0100

    Changed codecov config to increase acceptable change threshold

--- src/main/java/com/github/psambit9791/jdsp/filter/Butterworth.java
+++ src/main/java/com/github/psambit9791/jdsp/filter/Butterworth.java
@@ -29,7 +29,7 @@ public class Butterworth {
     /**
      * This method implements a low pass filter with given parameters, filters the signal and returns it.
      * @param order Order of the filter
-     * @param cutoffFreq The cutoff frequency for the filter
+     * @param cutoffFreq The cutoff frequency for the filter in Hz
      * @return double[] Filtered signal
      */
     public double[] low_pass_filter(int order, double cutoffFreq) {
@@ -45,7 +45,7 @@ public class Butterworth {
     /**
      * This method implements a high pass filter with given parameters, filters the signal and returns it.
      * @param order Order of the filter
-     * @param cutoffFreq The cutoff frequency for the filter
+     * @param cutoffFreq The cutoff frequency for the filter in Hz
      * @return double[] Filtered signal
      */
     public double[] high_pass_filter(int order, double cutoffFreq) {
@@ -61,8 +61,8 @@ public class Butterworth {
     /**
      * This method implements a band pass filter with given parameters, filters the signal and returns it.
      * @param order Order of the filter
-     * @param lowCutoff The lower cutoff frequency for the filter
-     * @param highCutoff The upper cutoff frequency for the filter
+     * @param lowCutoff The lower cutoff frequency for the filter in Hz
+     * @param highCutoff The upper cutoff frequency for the filter in Hz
      * @return double[] Filtered signal
      */
     public double[] band_pass_filter(int order, double lowCutoff, double highCutoff) {
@@ -80,8 +80,8 @@ public class Butterworth {
     /**
      * This method implements a band stop filter with given parameters, filters the signal and returns it.
      * @param order Order of the filter
-     * @param lowCutoff The lower cutoff frequency for the filter
-     * @param highCutoff The upper cutoff frequency for the filter
+     * @param lowCutoff The lower cutoff frequency for the filter in Hz
+     * @param highCutoff The upper cutoff frequency for the filter in Hz
      * @return double[] Filtered signal
      */
     public double[] band_stop_filter(int order, double lowCutoff, double highCutoff) {
