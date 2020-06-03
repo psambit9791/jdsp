a8dd804f0d2623b531f68ca255e7c40bc76a2dfa
psambit9791 <sambitpaul1992@gmail.com>
2020-06-03 21:35:37 +0100

    Removed plot display from test which was causing automated test fails on Travis

--- src/test/java/com/github/psambit9791/jdsp/TestLinePlot.java
+++ src/test/java/com/github/psambit9791/jdsp/TestLinePlot.java
@@ -53,7 +53,6 @@ public class TestLinePlot {
         fig.initialise_plot();
         fig.add_signal("Signal 1", signal1, true);
         fig.add_signal("Signal 2", signal2, true);
-        fig.plot();
         fig.save_as_png(outputFileName);
         boolean fileExists = new File("./"+outputFileName+".png").exists();
         assertTrue(fileExists);
