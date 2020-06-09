dc0ab8f35a0c917cdf5cd491e84e360a35ca848a
psambit9791 <sambitpaul1992@gmail.com>
2020-06-09 20:24:36 +0100

    Added test for multiplots with different y-axis

--- src/test/java/com/github/psambit9791/jdsp/TestPlotting.java
+++ src/test/java/com/github/psambit9791/jdsp/TestPlotting.java
@@ -22,11 +22,29 @@ public class TestPlotting {
 
     @Test
     public void testPlotSave1() throws IOException {
-        final double[] time = {0.0, 1.0, 2.0, 3.0, 4.0};
+        final double[] time1 = {-2.0, -1.0, 4.0, 5.0, 10.0};
+        final double[] time2 = {0.0, 1.0, 2.0, 3.0, 4.0};
         final double[] signal1 = {2.0, 4.0, 2.0, 3.0, 1.0};
         final double[] signal2 = {3.4, 6.7, 2.2, 1.6, 3.6};
 
         String outputFileName = "test_outputs/temp1";
+
+        Plotting fig = new Plotting("Sample Figure", "Time", "Signal");
+        fig.initialise_plot();
+        fig.add_signal("Signal 1", time1, signal1, true);
+        fig.add_signal("Signal 2", time2, signal2, true);
+        fig.save_as_png(outputFileName);
+        boolean fileExists = new File("./"+outputFileName+".png").exists();
+        Assertions.assertTrue(fileExists);
+    }
+
+    @Test
+    public void testPlotSave2() throws IOException {
+        final double[] time = {-2.0, -1.0, 4.0, 5.0, 10.0};
+        final double[] signal1 = {2.0, 4.0, 2.0, 3.0, 1.0};
+        final double[] signal2 = {3.4, 6.7, 2.2, 1.6, 3.6};
+
+        String outputFileName = "test_outputs/temp2";
 
         Plotting fig = new Plotting("Sample Figure", "Time", "Signal");
         fig.initialise_plot();
@@ -38,12 +56,12 @@ public class TestPlotting {
     }
 
     @Test
-    public void testPlotSave2() throws IOException {
+    public void testPlotSave3() throws IOException {
         final double[] time = {0.0, 1.0, 2.0, 3.0, 4.0};
         final double[] signal1 = {2.0, 4.0, 2.0, 3.0, 1.0};
         final double[] signal2 = {3.4, 6.7, 2.2, 1.6, 3.6};
 
-        String outputFileName = "test_outputs/temp2";
+        String outputFileName = "test_outputs/temp3";
 
         Plotting fig = new Plotting();
         fig.initialise_plot();
@@ -55,11 +73,11 @@ public class TestPlotting {
     }
 
     @Test
-    public void testPlotSave3() throws IOException {
+    public void testPlotSave4() throws IOException {
         final double[] signal1 = {2.0, 4.0, 2.0, 3.0, 1.0};
         final double[] signal2 = {3.4, 6.7, 2.2, 1.6, 3.6};
 
-        String outputFileName = "test_outputs/temp3";
+        String outputFileName = "test_outputs/temp4";
 
         Plotting fig = new Plotting(600, 500, "Sample Figure", "Time", "Signal");
         fig.initialise_plot();
@@ -71,14 +89,14 @@ public class TestPlotting {
     }
 
     @Test
-    public void testPlotSave4() throws IOException {
+    public void testPlotSave5() throws IOException {
         final double[] signal = {2.0, 4.0, 2.0, 3.0, 1.0};
         final double[] points1 = {3.4, 6.7, 2.2, 1.6, 3.6};
         final double[] points2 = {1.6, 2.0, 5.3, -1.3, 2.2};
 
         final double[] time = {0.0, 1.0, 2.0, 3.0, 4.0};
 
-        String outputFileName = "test_outputs/temp4";
+        String outputFileName = "test_outputs/temp5";
 
         Plotting fig = new Plotting(600, 500, "Sample Figure", "Time", "Signal");
         fig.initialise_plot();
