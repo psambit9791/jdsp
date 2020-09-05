bf3b1f6dac143ad20af9e02f7b9b5edd7a5461d5
psambit9791 <sambitpaul1992@gmail.com>
2020-09-05 13:36:09 +0100

    Added WAV IO support files from http://www.labbookpages.co.uk/

---
+++ src/main/java/com/github/psambit9791/jdsp/io/WavFileException.java
@@ -0,0 +1,24 @@
+package com.github.psambit9791.jdsp_test;
+
+public class WavFileException extends Exception
+{
+	public WavFileException()
+	{
+		super();
+	}
+
+	public WavFileException(String message)
+	{
+		super(message);
+	}
+
+	public WavFileException(String message, Throwable cause)
+	{
+		super(message, cause);
+	}
+
+	public WavFileException(Throwable cause) 
+	{
+		super(cause);
+	}
+}
