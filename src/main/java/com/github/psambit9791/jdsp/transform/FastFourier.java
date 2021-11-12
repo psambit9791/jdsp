a9eebc3b7a63d06a97b903845a03db831b2d8c04
psambit9791 <sambitpaul1992@gmail.com>
2021-11-12 01:30:56 +0000

    [fixes #19] Added classes for FastFourier and InverseFastFourier which uses the Apache Math3 version of FFT to speed up DFT compute

--- src/main/java/com/github/psambit9791/jdsp/transform/FastFourier.java
+++ src/main/java/com/github/psambit9791/jdsp/transform/FastFourier.java
@@ -1,66 +1,55 @@
 /*
- * Copyright (c) 2020 Sambit Paul
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *  * Copyright (c) 2020 Sambit Paul
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package com.github.psambit9791.jdsp.transform;
 
 import com.github.psambit9791.jdsp.misc.UtilMethods;
 import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.DftNormalization;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
 
 import java.util.Arrays;
 
-
-/**
- * <h1>Discrete Fourier Transform</h1>
- * The DiscreteFourier class applies the discrete fourier transform on the input signal and
- * provides different representations of the output to be returned (absolute values or complex values)
- * and if the output should be mirrored or not-mirrored
- * Reference <a href="https://mathworld.wolfram.com/DiscreteFourierTransform.html">article</a> for more information on discrete fourier transform.
- * <p>
- *
- * @author  Sambit Paul
- * @version 1.2
- */
-public class DiscreteFourier {
+public class FastFourier {
 
     private double[] signal;
-    private Complex[] output = null;
+    private Complex[] output;
+    private FastFourierTransformer ft;
 
-    /**
-     * This constructor initialises the prerequisites required to use DiscreteFourier.
-     * @param s Signal to be transformed
-     */
-    public DiscreteFourier(double[] s) {
-        this.signal = s;
-    }
-
-    /**
-     * This function performs the fourier transform on the input signal
-     */
-    public void dft() {
-        Complex[] out = new Complex[this.signal.length];
-
-        for (int k=0; k<out.length; k++) {
-            double real = 0;
-            double imag = 0;
-            for (int t=0; t<out.length; t++) {
-                double angle = (2*Math.PI*t*k)/out.length;
-                real += this.signal[t]*Math.cos(angle);
-                imag += -this.signal[t]*Math.sin(angle);
-            }
-            out[k] = new Complex(real, imag);
+    private void extendSignal() {
+        double power = Math.log(this.signal.length)/Math.log(2);
+        double raised_power = Math.ceil(power);
+        int new_length = (int)(Math.pow(2, raised_power));
+        if (new_length != this.signal.length) {
+            this.signal = UtilMethods.zeroPadSignal(this.signal, new_length);
         }
-        this.output = out;
     }
 
-    // Refer to this post to know the relevance of only positive: https://dsp.stackexchange.com/a/4827
-    // About plotting, please refer here: https://stackoverflow.com/a/25735274
+    public FastFourier(double[] signal) {
+        this.signal = signal;
+        this.extendSignal();
+        this.ft = new FastFourierTransformer(DftNormalization.STANDARD);
+    }
+
+    public FastFourier(double[] signal, DftNormalization norm) {
+        this.signal = signal;
+        this.extendSignal();
+        this.ft = new FastFourierTransformer(norm);
+    }
+
+    public void fft() {
+        this.output = this.ft.transform(this.signal, TransformType.FORWARD);
+    }
 
     /**
      * Returns the magnitude of the discrete fourier transformed sequence
@@ -145,14 +134,14 @@ public class DiscreteFourier {
     }
 
     /**
-     * Returns the complex value of the discrete fourier transformed sequence
+     * Returns the complex value of the fast fourier transformed sequence
      * @param onlyPositive Set to True if non-mirrored output is required
      * @throws java.lang.ExceptionInInitializerError if called before executing dft() method
      * @return Complex[] The complex DFT output
      */
     public Complex[] getComplex(boolean onlyPositive) throws ExceptionInInitializerError {
         if (this.output == null) {
-            throw new ExceptionInInitializerError("Execute dft() function before returning result");
+            throw new ExceptionInInitializerError("Execute fft() function before returning result");
         }
         Complex[] dftout;
 
---
+++ src/main/java/com/github/psambit9791/jdsp/transform/InverseFastFourier.java
@@ -0,0 +1,148 @@
+/*
+ *
+ *  * Copyright (c) 2020 Sambit Paul
+ *  *
+ *  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *  *
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package com.github.psambit9791.jdsp.transform;
+
+import org.apache.commons.math3.complex.Complex;
+import org.apache.commons.math3.transform.DftNormalization;
+import org.apache.commons.math3.transform.FastFourierTransformer;
+import org.apache.commons.math3.transform.TransformType;
+
+public class InverseFastFourier {
+
+    private Complex[] sequence;
+    private Complex[] signal = null;
+    private FastFourierTransformer ft;
+
+    private void checkOutput() {
+        if (this.signal == null) {
+            throw new ExceptionInInitializerError("Execute idft() function before returning result");
+        }
+    }
+
+    private Complex[] toFullSequence(Complex[] signal) {
+        int size = (signal.length-1)*2;
+        Complex[] out = new Complex[size];
+
+        System.arraycopy(out, 0, signal, 0, signal.length-1);
+        int index = signal.length;
+        for (int i=signal.length-2; i>0; i--) {
+            out[index] = signal[i].conjugate();
+        }
+        return signal;
+    }
+
+    public InverseFastFourier(Complex[] fftOutput, boolean onlyPositive) {
+        if (onlyPositive) {
+            this.sequence = this.toFullSequence(fftOutput);
+        }
+        else {
+            this.sequence = fftOutput;
+        }
+        this.ft = new FastFourierTransformer(DftNormalization.STANDARD);
+    }
+
+    public InverseFastFourier(Complex[] fftOutput, boolean onlyPositive, DftNormalization norm) {
+        if (onlyPositive) {
+            this.sequence = this.toFullSequence(fftOutput);
+        }
+        else {
+            this.sequence = fftOutput;
+        }
+        this.ft = new FastFourierTransformer(norm);
+    }
+
+    public void ifft() {
+        this.signal = this.ft.transform(this.sequence, TransformType.INVERSE);
+    }
+
+    /**
+     * This method returns the complex value of the generated signal as a Complex array.
+     * @throws java.lang.ExceptionInInitializerError if called before executing idft() method
+     * @return Complex[] The signal (complex)
+     */
+    public Complex[] getComplex() throws ExceptionInInitializerError {
+        this.checkOutput();
+        return this.signal;
+    }
+
+    /**
+     * This method returns the complex value of the generated signal as a 2D matrix.
+     * @throws java.lang.ExceptionInInitializerError if called before executing idft() method
+     * @return double[][] The signal (complex)
+     */
+    public double[][] getComplex2D() throws ExceptionInInitializerError {
+        checkOutput();
+        double[][] ret = new double[this.signal.length][2];
+        for (int i=0; i<ret.length; i++) {
+            ret[i][0] = this.signal[i].getReal();
+            ret[i][1] = this.signal[i].getImaginary();
+        }
+        return ret;
+    }
+
+    /**
+     * This method returns the real part of the generated signal.
+     * @throws java.lang.ExceptionInInitializerError if called before executing idft() method
+     * @return double[] The signal (real part)
+     */
+    public double[] getReal() throws ExceptionInInitializerError {
+        this.checkOutput();
+        double[] real = new double[this.signal.length];
+        for (int i=0; i<real.length; i++) {
+            real[i] = this.signal[i].getReal();
+        }
+        return real;
+    }
+
+    /**
+     * This method returns the imaginary part of the generated signal.
+     * @throws java.lang.ExceptionInInitializerError if called before executing idft() method
+     * @return double[] The signal (imaginary part)
+     */
+    public double[] getImaginary() throws ExceptionInInitializerError {
+        this.checkOutput();
+        double[] imag = new double[this.signal.length];
+        for (int i=0; i<imag.length; i++) {
+            imag[i] = this.signal[i].getImaginary();
+        }
+        return imag;
+    }
+
+    /**
+     * This method returns the magnitude value of the IDFT result.
+     * @throws java.lang.ExceptionInInitializerError if called before executing idft() method
+     * @return double[] The signal (magnitude)
+     */
+    public double[] getMagnitude() throws ExceptionInInitializerError {
+        checkOutput();
+        double[] ret = new double[this.signal.length];
+        for (int i=0; i<ret.length; i++) {
+            ret[i] = this.signal[i].abs();
+        }
+        return ret;
+    }
+
+    /**
+     * This method returns the phase value of the IDFT result.
+     * @throws java.lang.ExceptionInInitializerError if called before executing idft() method
+     * @return double[] phase of the signal
+     */
+    public double[] getPhase() throws ExceptionInInitializerError {
+        checkOutput();
+        double[] ret = new double[this.signal.length];
+        for (int i=0; i<ret.length; i++) {
+            ret[i] = this.signal[i].getArgument();
+        }
+        return ret;
+    }
+}
